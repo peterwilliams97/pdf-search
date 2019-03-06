c8ba2a15ad6528b6f3e0b02d42d38da20f8a7f74
Peter Williams <peter.williams@papercut.com>
2019-03-06 17:01:55 +1100

    location_index.go is building an index on disk

--- utils/locations_store.go
+++ utils/locations_store.go
@@ -24,7 +24,7 @@ import (
    Locations are read from disk a page at a time by ReadLocations which returns the
    []DocPageLocations for the PDF page given by `doc` and `page`.
 
-   func (hs *LocationsState) ReadLocations(doc uint64, page uint32) ([]DocPageLocations, error)
+   func (lState *LocationsState) ReadLocations(doc uint64, page uint32) ([]DocPageLocations, error)
 
    We use this to allow an efficient look up of DocPageLocation of an offset within a page's text.
    1) Look up []DocPageLocations for the PDF page given by `doc` and `page`
@@ -55,30 +55,26 @@ type LocationsState struct {
 	root         string            // top level directory of the data saved to disk
 	fileList     []FileDesc        // list of file entries
 	hashIndex    map[string]uint64 // {file hash: index into fileList}
+	indexHash    map[uint64]string // {index into fileList: file hash}
 	hashPath     map[string]string // {file hash: file path}
 	updateTime   time.Time
 	locationsDir string
 }
 
-// FileDesc describes a PDF file.
-type FileDesc struct {
-	InPath string // Full path to PDF file.
-	Hash   string // SHA-256 hash of file contents
-	SizeMB float64
-}
-
 // OpenLocationsState loads indexe from an existing locations directory `root`.
 func OpenLocationsState(root string) (*LocationsState, error) {
-	hs := LocationsState{root: root}
-	filename := hs.fileListPath()
+	lState := LocationsState{root: root}
+	filename := lState.fileListPath()
 	fileList, err := loadFileList(filename)
 	if err != nil {
 		return nil, err
 	}
 	hashIndex := map[string]uint64{}
+	indexHash := map[uint64]string{}
 	hashPath := map[string]string{}
 	for i, hip := range fileList {
 		hashIndex[hip.Hash] = uint64(i)
+		indexHash[uint64(i)] = hip.Hash
 		hashPath[hip.Hash] = hip.InPath
 	}
 
@@ -86,23 +82,27 @@ func OpenLocationsState(root string) (*LocationsState, error) {
 		root:      root,
 		fileList:  fileList,
 		hashIndex: hashIndex,
+		indexHash: indexHash,
 		hashPath:  hashPath,
 	}, nil
 }
 
-func (hs *LocationsState) ExtractDocPagesLookup2(inPath string) ([]Loc2Page, error) {
+func (lState *LocationsState) ExtractDocPagesLookup2(inPath string) ([]Loc2Page, error) {
 	fd, err := CreateFileDesc(inPath)
 	if err != nil {
+		panic(err)
 		return nil, err
 	}
-	docIdx, p, exists := hs.AddFile(fd)
+	docIdx, p, exists := lState.AddFile(fd)
 	if exists {
 		common.Log.Error("ExtractDocPagesLookup2: %q is the same PDF as %q. Ignoring", inPath, p)
+		panic(err)
 		return nil, errors.New("duplicate PDF")
 	}
 
-	ls, err := hs.CreateLocationsStore(docIdx)
+	lDoc, err := lState.CreateLocationsDoc(docIdx)
 	if err != nil {
+		panic(err)
 		return nil, err
 	}
 
@@ -113,14 +113,21 @@ func (hs *LocationsState) ExtractDocPagesLookup2(inPath string) ([]Loc2Page, err
 		if err != nil {
 			common.Log.Error("ExtractDocPagesLookup2: ExtractPageTextLocation failed. inPath=%q pageNum=%d err=%v",
 				inPath, pageNum, err)
+			panic(err)
 			return err
 		}
 		if text == "" {
 			return nil
 		}
 
-		pageIdx, err := ls.AddPage(locations)
+		var dpl serial.DocPageLocations
+		for _, loc := range locations {
+			dpl.Locations = append(dpl.Locations, ToSerialTextLocation(loc))
+		}
+
+		pageIdx, err := lDoc.AddPage(dpl)
 		if err != nil {
+			panic(err)
 			return err
 		}
 		docPages = append(docPages, Loc2Page{
@@ -136,14 +143,250 @@ func (hs *LocationsState) ExtractDocPagesLookup2(inPath string) ([]Loc2Page, err
 	if err != nil {
 		return docPages, err
 	}
-	err = ls.SaveLocations(hash, docPages)
+	err = lDoc.Close()
+	if err != nil {
+		panic(err)
+	}
 	return docPages, err
 }
 
-type Loc2Page struct {
-	Doc      uint64 // Doc index.
-	Page     uint32 // Page index.
-	Contents string // Page text. !@#$ -> Text
+// // ReadLocations returns the DocPageLocations for the PDF page given by `doc`, `page`.
+// func (lState *LocationsState) ReadLocations(doc uint64, page uint32) ([]serial.DocPageLocations, error) {
+// 	hash, ok := lState.indexHash[doc]
+// 	if !ok {
+// 		panic("XXXX")
+// 	}
+// 	locationsPath := lState.locationsPath(hash)
+// 	locationsFile, err := os.Open(locationsPath)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Could not open locations file %q.\n", locationsPath)
+// 		panic(err)
+// 	}
+// 	defer locationsFile.Close()
+
+// 	var dplList []serialDocPageLocation
+// 	for _, l := range docPages {
+// 		// dpl := l.ToDocPageLocations()
+// 		dpl, err := serial.RReadDocPageLocations(locationsFile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		dplList = append(dplList, dpl)
+// 	}
+// 	return dplList, nil
+// }
+
+// addFile adds PDF file `fd` to `lState`. fileList.
+func (lState *LocationsState) AddFile(fd FileDesc) (uint64, string, bool) {
+	hash := fd.Hash
+	idx, ok := lState.hashIndex[hash]
+	if ok {
+		return idx, lState.hashPath[hash], true
+	}
+	lState.fileList = append(lState.fileList, fd)
+	idx = uint64(len(lState.fileList) - 1)
+	lState.hashIndex[hash] = idx
+	lState.indexHash[idx] = hash
+	lState.hashPath[hash] = fd.InPath
+	dt := time.Since(lState.updateTime)
+	// fmt.Fprintf(os.Stderr, "*00 Flush: %s %.1f sec\n", lState.updateTime, dt.Seconds())
+	if dt.Seconds() > hashUpdatePeriodSec {
+		lState.Flush()
+		fmt.Fprintf(os.Stderr, "*** Flush: %s (%.1f sec) %d elements\n",
+			lState.updateTime, dt.Seconds(), idx)
+		lState.updateTime = time.Now()
+	}
+	return idx, fd.InPath, false
+}
+
+func (lState *LocationsState) Flush() error {
+	return saveFileList(lState.fileListPath(), lState.fileList)
+}
+
+// fileListPath is the path where lState.fileList is stored on disk.
+func (lState *LocationsState) fileListPath() string {
+	return filepath.Join(lState.root, "file_list.json")
+}
+
+// locationsPath returns the file path to the locations file for PDF with hash `hash`.
+func (lState *LocationsState) locationsPath(hash string) string {
+	return filepath.Join(lState.locationsDir, hash)
+}
+
+func (lState *LocationsState) createIfNecessary() error {
+	common.Log.Info("createIfNecessary: 1 locationsDir=%q", lState.locationsDir)
+	if lState.locationsDir != "" {
+		return nil
+	}
+	lState.locationsDir = filepath.Join(lState.root, "locations")
+	common.Log.Info("createIfNecessary: 2 locationsDir=%q", lState.locationsDir)
+	err := MkDir(lState.locationsDir)
+	common.Log.Info("createIfNecessary: err=%v", err)
+	return err
+}
+
+//
+//
+//
+type IndexEntry struct {
+	Offset uint32
+	Size   uint32
+	Check  uint32
+}
+
+type LocationsDoc struct {
+	lState    *LocationsState
+	dataFile  *os.File
+	index     []IndexEntry
+	dataPath  string
+	indexPath string
+}
+
+func (lState *LocationsState) CreateLocationsDoc(docIdx uint64) (*LocationsDoc, error) {
+
+	err := lState.createIfNecessary()
+	if err != nil {
+		panic(err)
+	}
+	hash := lState.fileList[docIdx].Hash
+	locPath := lState.locationsPath(hash)
+	dataPath := locPath + ".dat"
+	indexPath := locPath + ".idx.json"
+	indexPath, err = filepath.Abs(indexPath)
+	if err != nil {
+		panic(err)
+	}
+	dataPath, err = filepath.Abs(dataPath)
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create(dataPath)
+	if err != nil {
+		panic(err)
+		return nil, err
+	}
+	return &LocationsDoc{
+		lState:    lState,
+		dataFile:  f,
+		dataPath:  dataPath,
+		indexPath: indexPath,
+		// index:     []IndexEntry{},
+	}, nil
+}
+
+// func (lState *LocationsState) OpenLocationsDoc(storeDir string) (*LocationsDoc, error) {
+// 	dataPath := filepath.Join(storeDir, "dat")
+// 	indexPath := filepath.Join(storeDir, "idx")
+// 	buf, err := ioutil.ReadFile(indexPath)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	var index []IndexEntry
+// 	if err := json.Unmarshal(buf, &index); err != nil {
+// 		return nil, err
+// 	}
+// 	f, err := os.Open(dataPath)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &LocationsDoc{
+// 		lState:    lState,
+// 		dataFile:  f,
+// 		index:     index,
+// 		indexPath: indexPath,
+// 		dataPath:  dataPath,
+// 	}, nil
+// }
+
+func (lDoc *LocationsDoc) Close() error {
+
+	lDoc.dataFile.Close()
+
+	b, err := json.MarshalIndent(lDoc.index, "", "\t")
+	// fmt.Printf("lDoc.index=%#v\n", lDoc.index)
+	// fmt.Printf("b=%s\n", string(b))
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(lDoc.indexPath, b, 0666)
+
+	// // if err := lState.createIfNecessary(); err != nil {
+	// // 	return err
+	// // }
+	// locationsPath := lDoc.dataPath
+	// locationsFile, err := os.Create(locationsPath)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Could not create locations file %q.\n", locationsPath)
+	// 	panic(err)
+	// }
+	// defer locationsFile.Close()
+
+	// for _, l := range docPages {
+	// 	dpl := l.ToDocPageLocations()
+	// 	if err := serial.WriteDocPageLocations(locationsFile, dpl); err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	// indexPath := filepath.Join(locPath, "idx")
+
+	// return nil
+}
+
+// func (lDoc *LocationsDoc) Close() error {
+// 	for _, l := range lDoc.docPages {
+// 		dpl := l.ToDocPageLocations()
+// 		if err := serial.WriteDocPageLocations(locationsFile, dpl); err != nil {
+// 			panic(err)
+// 		}
+// 	}
+
+// 	if buf, err := json.MarshalIndent(lDoc.index, "", "\t"); err != nil {
+// 		return err
+// 	}
+// 	return lDoc.dataFile.Close()
+// }
+
+func (lDoc *LocationsDoc) AddPage(dpl serial.DocPageLocations) (uint32, error) {
+	b := flatbuffers.NewBuilder(0)
+	buf := serial.MakeDocPageLocations(b, dpl)
+	check := crc32.ChecksumIEEE(buf) // uint32
+	offset, err := lDoc.dataFile.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, err
+	}
+
+	e := IndexEntry{
+		Offset: uint32(offset),
+		Size:   uint32(len(buf)),
+		Check:  check,
+	}
+
+	if _, err := lDoc.dataFile.Write(buf); err != nil {
+		return 0, err
+	}
+	lDoc.index = append(lDoc.index, e)
+	return uint32(len(lDoc.index) - 1), nil
+}
+
+func (lDoc *LocationsDoc) ReadDocPageLocations2(idx int) (serial.DocPageLocations, error) {
+	e := lDoc.index[idx]
+	lDoc.dataFile.Seek(io.SeekStart, int(e.Offset))
+	buf := make([]byte, e.Size)
+	if _, err := lDoc.dataFile.Read(buf); err != nil {
+		return serial.DocPageLocations{}, err
+	}
+	if crc32.ChecksumIEEE(buf) != e.Check {
+		return serial.DocPageLocations{}, errors.New("bad checksum")
+	}
+	return serial.ReadDocPageLocations(buf)
+}
+
+// FileDesc describes a PDF file.
+type FileDesc struct {
+	InPath string // Full path to PDF file.
+	Hash   string // SHA-256 hash of file contents
+	SizeMB float64
 }
 
 func CreateFileDesc(inPath string) (FileDesc, error) {
@@ -151,86 +394,12 @@ func CreateFileDesc(inPath string) (FileDesc, error) {
 	if err != nil {
 		return FileDesc{}, err
 	}
-	size, err := FileSize(inPath)
-	if err != nil {
-		return FileDesc{}, err
-	}
+	size := FileSize(inPath)
 	return FileDesc{
 		InPath: inPath,
 		Hash:   hash,
 		SizeMB: float64(size) / 1024.0 / 1024.0,
 	}, nil
-}
-
-// fileListPath is the path where hs.fileList is stored on disk.
-func (hs *LocationsState) fileListPath() string {
-	return filepath.Join(hs.root, "file_list.json")
-}
-
-// locationsPath returns the file path to the locations file for PDF with hash `hash`.
-func (hs *LocationsState) locationsPath(hash string) string {
-	return filepath.Join(hs.locationsDir, hash)
-}
-
-func (hs *LocationsState) createIfNecessary() error {
-	common.Log.Info("createIfNecessary: 1 locationsDir=%q", hs.locationsDir)
-	if hs.locationsDir != "" {
-		return nil
-	}
-	hs.locationsDir = filepath.Join(hs.root, "locations")
-	common.Log.Info("createIfNecessary: 2 locationsDir=%q", hs.locationsDir)
-	err := MkDir(hs.locationsDir)
-	common.Log.Info("createIfNecessary: err=%v", err)
-	return err
-}
-
-// ReadLocations returns the DocPageLocations for the PDF page given by `doc`, `page`.
-func (hs *LocationsState) ReadLocations(doc uint64, page uint32) ([]DocPageLocations, error) {
-	hash := hs.indexHash[doc]
-	locationsPath := hs.locationsPath(hash)
-	locationsFile, err := os.Open(locationsPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not open locations file %q.\n", locationsPath)
-		panic(err)
-	}
-	defer locationsFile.Close()
-
-	var dplList []DocPageLocation
-	for _, l := range docPages {
-		// dpl := l.ToDocPageLocations()
-		dpl, err := serial.RReadDocPageLocations(locationsFile)
-		if err != nil {
-			panic(err)
-		}
-		dplList = append(dplList, dpl)
-	}
-	return dplList, nil
-}
-
-// addFile adds PDF file `fd` to `hs`. fileList.
-func (hs *LocationsState) AddFile(fd FileDesc) (uint64, string, bool) {
-	hash := fd.Hash
-	idx, ok := hs.hashIndex[hash]
-	if ok {
-		return idx, hs.hashPath[hash], true
-	}
-	hs.fileList = append(hs.fileList, fd)
-	idx = uint64(len(hs.fileList) - 1)
-	hs.hashIndex[hash] = idx
-	hs.hashPath[hash] = inPath
-	dt := time.Since(hs.updateTime)
-	// fmt.Fprintf(os.Stderr, "*00 Flush: %s %.1f sec\n", hs.updateTime, dt.Seconds())
-	if dt.Seconds() > hashUpdatePeriodSec {
-		hs.Flush()
-		fmt.Fprintf(os.Stderr, "*** Flush: %s (%.1f sec) %d elements\n",
-			hs.updateTime, dt.Seconds(), idx)
-		hs.updateTime = time.Now()
-	}
-	return idx, inPath, false
-}
-
-func (hs *LocationsState) Flush() error {
-	return saveFileList(hs.fileListPath(), hs.fileList)
 }
 
 func loadFileList(filename string) ([]FileDesc, error) {
@@ -254,6 +423,12 @@ func saveFileList(filename string, fileList []FileDesc) error {
 	return ioutil.WriteFile(filename, b, 0666)
 }
 
+type Loc2Page struct {
+	Doc      uint64 // Doc index.
+	Page     uint32 // Page index.
+	Contents string // Page text. !@#$ -> Text
+}
+
 func (l Loc2Page) ID() string {
 	return fmt.Sprintf("%04X.%d", l.Doc, l.Page)
 }
@@ -272,129 +447,4 @@ func ToSerialTextLocation(loc extractor.TextLocation) serial.TextLocation {
 		Urx:    float32(b.Urx),
 		Ury:    float32(b.Ury),
 	}
-}
-
-//
-//
-//
-type IndexEntry struct {
-	offset uint32
-	size   uint32
-	check  uint32
-}
-
-type LocationsStore struct {
-	hs       *LocationsState
-	dataFile *os.File
-	index    []IndexEntry
-}
-
-func (hs *LocationsState) CreateLocationsStore(docIdx uint64) (*LocationsStore, error) {
-	hash := hs.FileList[docIdx].Hash
-	locPath := hs.locationsPath(hash)
-	dataPath := filepath.Join(locPath, "dat")
-	f, err := os.Create(dataPath)
-	if err != nil {
-		return nil, err
-	}
-	return &LocationsStore{
-		hs:       hs,
-		dataFile: f,
-	}, nil
-}
-
-func (hs *LocationsState) OpenLocationsStore(storeDir string) (*LocationsStore, error) {
-	dataPath := filepath.Join(storeDir, "data")
-	indexPath := filepath.Join(storeDir, "index")
-	buf, err := ioutil.ReadFile(indexPath)
-	if err != nil {
-		return nil, err
-	}
-	var index []IndexEntry
-	if err := json.Unmarshal(buf, &index); err != nil {
-		return nil, err
-	}
-	f, err := os.Open(dataPath)
-	if err != nil {
-		return nil, err
-	}
-	return LocationsStore{
-		storeDir: storeDir,
-		dataFile: f,
-		index:    index,
-	}, nil
-}
-
-func (ls *LocationsStore) Close(hash string, docPages []Loc2Page) error {
-
-	// if err := hs.createIfNecessary(); err != nil {
-	// 	return err
-	// }
-	locationsPath := filepath.Join(hs.locationsDir, hash)
-	locationsFile, err := os.Create(locationsPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not create locations file %q.\n", locationsPath)
-		panic(err)
-	}
-	defer locationsFile.Close()
-
-	for _, l := range docPages {
-		dpl := l.ToDocPageLocations()
-		if err := serial.WriteDocPageLocations(locationsFile, dpl); err != nil {
-			panic(err)
-		}
-	}
-
-	indexPath := filepath.Join(locPath, "idx")
-
-	return nil
-}
-
-func (ls *LocationsStore) Close() error {
-	for _, l := range ls.docPages {
-		dpl := l.ToDocPageLocations()
-		if err := serial.WriteDocPageLocations(locationsFile, dpl); err != nil {
-			panic(err)
-		}
-	}
-
-	if buf, err := json.MarshalIndent(ls.index, "", "\t"); err != nil {
-		return err
-	}
-	return ls.dataFile.Close()
-}
-
-func (ls *LocationsStore) AddPage(dpl serial.DocPageLocations) (uint32, error) {
-	b := flatbuffers.NewBuilder(0)
-	buf := serial.MakeDocPageLocations(b, dpl)
-	check := crc32.ChecksumIEEE(buf) // uint32
-	offset, err := ls.dataFile.Seek(0, io.SeekCurrent)
-	if err != nil {
-		return 0, err
-	}
-
-	e := IndexEntry{
-		offset: uint32(offset),
-		size:   uint32(len(buf)),
-		check:  check,
-	}
-
-	if _, err := ls.dataFile.Write(buf); err != nil {
-		return 0, err
-	}
-	ls.index = append(ls.index, e)
-	return uint32(len(ls.index) - 1), nil
-}
-
-func (ls *LocationsStore) ReadDocPageLocations2(idx int) (serial.DocPageLocations, error) {
-	e := ls.index[idx]
-	ls.dataFile.Seek(io.SeekStart, int(e.offset))
-	buf := make([]byte, e.size)
-	if _, err := ls.dataFile.Read(buf); err != nil {
-		return serial.DocPageLocations{}, err
-	}
-	if crc32.ChecksumIEEE(buf) != e.check {
-		return serial.DocPageLocations{}, errors.New("bad checksum")
-	}
-	return serial.ReadDocPageLocations(buf)
 }
